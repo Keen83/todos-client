300026cc60f58fb2ce72dcbbeba0051c2a710b3a
Olexandr Nikolaevsky <ksander.nik@gmail.com>
2018-11-11 23:00:45 +0200

    Add top level components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DateComponent } from './date/date.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PaymentsComponent } from './payments/payments.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'date/:date', component: DateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'payments', component: PaymentsComponent },
+  // {path: 'setup', }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
